ab10b27bfeadf586899f29aad7618bb93f48957b
Connor Gaskill <gaskillconnor@gmail.com>
2025-11-16 22:08:44 -0800

    Add project files.

---
+++ Week 8 Debug/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Week_8_Debug
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+    }
+
+    class BrokenHashTable
+    {
+        private List<int>[] buckets;
+
+        public BrokenHashTable(int size)
+        {
+            buckets = new List<int>[size];
+        }
+
+        private int Hash(int key)
+        {
+            return key % buckets.Length;
+        }
+
+        public void Insert(int key)
+        {
+            int index = Hash(key);
+            if (buckets[index] == null)
+            {
+                buckets[index] = new List<int>();
+            }
+
+            // 🚨 Problem: resets the bucket every time, erasing old values
+            if (!buckets[index].Contains(key))
+            {
+                buckets[index] = new List<int>();
+                buckets[index].Add(key);
+            }
+        }
+
+        public bool Contains(int key)
+        {
+            int index = Hash(key);
+            return buckets[index] != null && buckets[index].Contains(key);
+        }
+    }
+
+}
